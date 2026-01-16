70491aa3bef2a325152ad67a4c6f487f893d5619
samuelchang678 <samuelchang678@gmail.com>
2026-01-16 13:44:07 +0800

    feat: bare functionality of notebook

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,164 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+import { Metadata } from 'next';
+import "./page.scss";
+
+export type Note = {
+  id: string;
+  title: string;
+  content: string;
+  updatedAt: number;
+};
+
+const STORAGE_KEY = "notes";
+
 
 export default function Home() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [activeNoteId, setActiveNoteId] = useState<string | null>(null);
+
+  // Load notes from localStorage
+  useEffect((): void => {
+    const saved = localStorage.getItem(STORAGE_KEY);
+    if (!saved) return;
+
+  try {
+    const parsed: Note[] = JSON.parse(saved);
+
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setNotes(parsed);
+
+    if (parsed.length > 0) {
+      setActiveNoteId((prevId) => prevId ?? parsed[0].id);
+    }
+  } catch {
+    console.error("Failed to parse notes from localStorage");
+  }
+}, []);
+
+  // Save notes to localStorage
+  useEffect((): void => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(notes));
+  }, [notes]);
+
+  const activeNote: Note | undefined = notes.find(
+    (note) => note.id === activeNoteId
+  );
+
+  const createNote = (): void => {
+    const newNote: Note = {
+      id: crypto.randomUUID(),
+      title: "Untitled",
+      content: "",
+      updatedAt: Date.now(),
+    };
+
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setActiveNoteId(newNote.id);
+  };
+
+  const updateNote = (content: string): void => {
+    setNotes((prevNotes) =>
+      prevNotes.map((note) =>
+        note.id === activeNoteId
+          ? { ...note, content, updatedAt: Date.now() }
+          : note
+      )
+    );
+  };
+
+  const updateNoteTitle = (id: string, newTitle: string) => {
+    setNotes((prevNotes) =>
+      prevNotes.map((note) =>
+        note.id === id ? { ...note, title: newTitle } : note
+      )
+    );
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="notebookContainer">
+      {/* Panel 1: Notes List */}
+      <div className="panel">
+        <button onClick={createNote}>+ New Note</button>
+        {notes.map((note: Note) => (
+          <div
+            key={note.id}
+            onClick={() => setActiveNoteId(note.id)}
+            className="noteContainer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            {note.id === activeNoteId ? (
+              <input
+                id="selected-note"
+                type="text"
+                value={note.title}
+                onChange={(e) => updateNoteTitle(note.id, e.target.value)}
+                autoFocus
+                className="noteSelected"
+              />
+            ) : (
+              <span className="noteUnselected">{note.title}</span>
+            )}
+          </div>
+        ))}
+      </div>
+      {/* Panel 2: Editor */}
+      <div className="panel">
+        {activeNote ? (
+          <textarea
+            value={activeNote.content}
+            onChange={(
+              e: React.ChangeEvent<HTMLTextAreaElement>
+            ) => updateNote(e.target.value)}
+            style={{
+              width: "100%",
+              height: "100%",
+              resize: "none",
+            }}
+          />
+        ) : (
+          <p>No note selected</p>
+        )}
+      </div>
+      {/* Panel 3: Markdown Preview */}
+      <div className="panel">
+        {activeNote ? (
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw]}
+             components={{
+                code(props) {
+                  const {children, className, node, ...rest} = props
+                  const match = /language-(\w+)/.exec(className || '')
+                  return match ? (
+                    <SyntaxHighlighter
+                      {...rest}
+                      PreTag="div"
+                      // eslint-disable-next-line react/no-children-prop
+                      children={String(children).replace(/\n$/, '')}
+                      language={match[1]}
+                      style={dark}
+                    />
+                  ) : (
+                    <code {...rest} className={className}>
+                      {children}
+                    </code>
+                  )
+                }
+              }}
           >
-            Documentation
-          </a>
-        </div>
-      </main>
+            {activeNote.content}
+          </ReactMarkdown>
+        ) : (
+          <p>Nothing to preview</p>
+        )}
+      </div>
     </div>
   );
 }
